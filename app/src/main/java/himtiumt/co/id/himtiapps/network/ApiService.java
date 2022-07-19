ea5c8b6c735731eddf920dee5f87109d5378bf41
Risky Budi <riskibudi2003.rb@gmail.com>
2022-07-19 20:06:30 +0700

    Progres anti revisi struktur organisasi TEAM 6

--- app/src/main/java/himtiumt/co/id/himtiapps/network/ApiService.java
+++ app/src/main/java/himtiumt/co/id/himtiapps/network/ApiService.java
@@ -2,13 +2,17 @@ package himtiumt.co.id.himtiapps.network;
 
 import himtiumt.co.id.himtiapps.authentication.login.model.RequestLogin;
 import himtiumt.co.id.himtiapps.authentication.login.model.ResponseLogin;
+import himtiumt.co.id.himtiapps.strukturOrganisasi.model.ResponseStrukturOrganisasi;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
     @POST("Api-login")
     Call<ResponseLogin> login(@Body RequestLogin requestLogin);
+    @GET("Api-struktur-himti")
+    Call<ResponseStrukturOrganisasi> getStrukturOrganisasi();
 
 }
