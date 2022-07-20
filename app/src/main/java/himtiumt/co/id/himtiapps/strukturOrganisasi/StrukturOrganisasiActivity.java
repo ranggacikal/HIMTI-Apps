ccd4efef01d9eb8f3c87d200fce00e1ceaab4c8e
dimasnoufal <dimasnoufal26@gmail.com>
2022-07-20 19:44:26 +0700

    Progress Struktur Organisasi (team 6)

--- app/src/main/java/himtiumt/co/id/himtiapps/strukturOrganisasi/StrukturOrganisasiActivity.java
+++ app/src/main/java/himtiumt/co/id/himtiapps/strukturOrganisasi/StrukturOrganisasiActivity.java
@@ -3,11 +3,14 @@ package himtiumt.co.id.himtiapps.strukturOrganisasi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.List;
 
 import himtiumt.co.id.himtiapps.databinding.ActivityStrukturOrganisasiBinding;
+import himtiumt.co.id.himtiapps.home.HomeActivity;
 import himtiumt.co.id.himtiapps.network.ApiConfig;
 import himtiumt.co.id.himtiapps.strukturOrganisasi.adapter.StrukturOrganisasiAdapter;
 import himtiumt.co.id.himtiapps.strukturOrganisasi.model.PengurusItem;
@@ -31,6 +34,14 @@ public class StrukturOrganisasiActivity extends AppCompatActivity {
         GridLayoutManager gridLayoutManagerBPH = new GridLayoutManager(StrukturOrganisasiActivity.this,
                 2, GridLayoutManager.VERTICAL, false);
         binding.rvStrukturOrganisasi.setLayoutManager(gridLayoutManagerBPH);
+
+        binding.tvStrukturOrganisasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(StrukturOrganisasiActivity.this, HomeActivity.class);
+                startActivity(back);
+            }
+        });
 
         ApiConfig.service.getStrukturOrganisasi().enqueue(new Callback<ResponseStrukturOrganisasi>() {
             @Override
