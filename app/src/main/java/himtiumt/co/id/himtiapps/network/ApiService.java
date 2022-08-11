2027a4ac3894b0653336cbc689cff92b0122422d
kaimdin <dhyflatupono1402>
2022-08-11 14:13:42 +0700

    Progres setting profile team 1

--- app/src/main/java/himtiumt/co/id/himtiapps/network/ApiService.java
+++ app/src/main/java/himtiumt/co/id/himtiapps/network/ApiService.java
@@ -2,13 +2,20 @@ package himtiumt.co.id.himtiapps.network;
 
 import himtiumt.co.id.himtiapps.authentication.login.model.RequestLogin;
 import himtiumt.co.id.himtiapps.authentication.login.model.ResponseLogin;
+import himtiumt.co.id.himtiapps.profile.model.RequestProfile;
+import himtiumt.co.id.himtiapps.profile.model.ResponseProfile;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
 
     @POST("Api-login")
     Call<ResponseLogin> login(@Body RequestLogin requestLogin);
 
+    @PATCH("Api-edit-profile/{id}")
+    Call <ResponseProfile> getprofilebyid (@Path("id") int postid,
+                                           @Body RequestProfile requestProfile);
 }
---
+++ app/src/main/java/himtiumt/co/id/himtiapps/profile/model/ResponseProfile.java
@@ -0,0 +1,39 @@
+package himtiumt.co.id.himtiapps.profile.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseProfile{
+
+	@SerializedName("profilUpdate")
+	private ProfilUpdate profilUpdate;
+
+	@SerializedName("message")
+	private String message;
+
+	@SerializedName("status")
+	private Boolean status;
+
+	public void setProfilUpdate(ProfilUpdate profilUpdate){
+		this.profilUpdate = profilUpdate;
+	}
+
+	public ProfilUpdate getProfilUpdate(){
+		return profilUpdate;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	public void setStatus(Boolean status){
+		this.status = status;
+	}
+
+	public Boolean isStatus(){
+		return status;
+	}
+}
